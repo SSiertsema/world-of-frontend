47efa97e10db4b0ad8f178ace53e24f8bf873a25
Sven Siertsema <sven.siertsema@vattenfall.com>
2020-05-11 20:51:29 +0200

    adds subscriptions

---
+++ store/emailSubscriptions.js
@@ -0,0 +1,40 @@
+export const state = () => ({
+  all: [
+    {
+      title: 'Frontend Focus',
+      description:
+        'A once–weekly roundup of the best front-end news, articles and tutorials. HTML, CSS, WebGL, Canvas, browser tech, and more.',
+      url: 'https://frontendfoc.us/'
+    },
+    {
+      title: 'Frontend Weekly',
+      description:
+        'The best articles, links and news related to Frontend Development delivered once a week to your inbox.',
+      url: 'https://frontendweekly.co/'
+    },
+    {
+      title: 'WDRL (Web deployment Reading list)',
+      description:
+        'A handcrafted, carefully created weekly update resource for web developers by Anselm Hannemann, founder of Colloq. The project is financed by your donations.',
+      url: 'https://wdrl.info/'
+    },
+    {
+      title: 'JavaScript Weekly',
+      description:
+        'A free, once–weekly email roundup of JavaScript news and articles.',
+      url: 'https://javascriptweekly.com/'
+    },
+    {
+      title: 'ES.next News',
+      description:
+        '5 ECMAScript.next links every week, in your inbox. Curated by Dr. Axel Rauschmayer and Johannes Weber',
+      url: 'http://esnextnews.com/'
+    },
+    {
+      title: 'CSS weekly',
+      description:
+        'weekly e-mail roundup of css articles, tutorials, experiments and tools curated by Zoran Jambor',
+      url: 'https://css-weekly.com/'
+    }
+  ]
+})
